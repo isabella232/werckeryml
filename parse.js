351ea9a23cc38453eeced9ec951b0092bde45e66
Benno van den Berg <bennovandenberg@gmail.com>
2015-10-08 15:24:56 +0200

    Add documentation and reorder methods

--- parse.js
+++ parse.js
@@ -20,6 +20,45 @@ module.exports = function(input) {
   return normalized;
 };
 
+// hasNullValue returns true if o[key] has a value of null.
+var hasNullValue = function(o, key) {
+  return o[key] === null;
+};
+
+// reservedRootKeys are the keys which should not be normalized as pipeline.
+var reservedRootKeys = [
+  'box',
+  'services',
+  'command-timeout',
+  'source-dir',
+  'no-response-timeout'
+];
+
+// shouldProcessPipelineKey returns true if key is not present in
+// reservedRootKeys.
+var shouldProcessPipelineKey = function(key) {
+  return reservedRootKeys.indexOf(key) === -1;
+};
+
+// reservedPipelineKeys are the keys which should not be normalized as extra
+// steps.
+var reservedPipelineKeys = [
+  'box',
+  'services',
+  'steps',
+  'after-steps'
+];
+
+// shouldProcessExtraStepsKey returns true if key is not present in
+// reservedPipelineKeys.
+var shouldProcessExtraStepsKey = function(key) {
+  return reservedPipelineKeys.indexOf(key) === -1;
+};
+
+// normalizeRoot normalize the root object of a wercker.yml. It will normalize
+// all expected properties to their normalized forms (see reserved
+// reservedRootKeys), and it will try to convert all non reserved keys to
+// pipelines. All pipelines will be stored in the pipelines property.
 var normalizeRoot = function(parsed) {
   if (parsed.box) {
     parsed.box = normalizeBox(parsed.box);
@@ -37,6 +76,14 @@ var normalizeRoot = function(parsed) {
   return parsed;
 };
 
+// normalizeBox normalizes a box property (found in box, services, and
+// pipelines) into the long format notation. It currently supports the
+// following input formats:
+// - Short form:
+// box: "ubuntu"
+// - Long form:
+// box:
+//   id: "ubuntu"
 var normalizeBox = function(box) {
   if (typeof box === 'string') {
     return {
@@ -47,34 +94,15 @@ var normalizeBox = function(box) {
   return box;
 };
 
+// normalizeServices takes an array of services, and runs them through
+// normalizeBox.
 var normalizeServices = function(services) {
   return services.map(normalizeBox);
 };
 
-var processPipelines = function(parsed) {
-  var pipelines = {};
-  var hasPipelines = false;
-
-  Object.keys(parsed)
-    .filter(shouldProcessPipelineKey)
-    .forEach(function(key) {
-      pipelines[key] = normalizePipeline(parsed[key]);
-      delete parsed[key];
-      hasPipelines = true;
-    });
-
-  if (hasPipelines) {
-    return pipelines;
-  }
-
-  return null;
-};
-
-var reservedRootKeys = ['box', 'services', 'command-timeout', 'source-dir', 'no-response-timeout'];
-var shouldProcessPipelineKey = function(key) {
-  return reservedRootKeys.indexOf(key) === -1;
-};
-
+// normalizePipeline takes an pipeline object and normalizes any properties.
+// If any extra steps are present, then they will be moved in the extraSteps
+// property.
 var normalizePipeline = function(pipeline) {
   if (!pipeline) {
     return null;
@@ -104,10 +132,21 @@ var normalizePipeline = function(pipeline) {
   return pipeline;
 };
 
+// normalizeSteps takes an array of steps, and runs them through normalizeStep.
 var normalizeSteps = function(steps) {
   return steps.map(normalizeStep);
 };
 
+// normalizeStep takes an step object and normalizes it to to include the id
+// property. The following formats are supported:
+// - Short form (just a string):
+// npm_install
+// - Long form (map where the properties are indented):
+// script:
+//   name: sample
+// - Long form (map where the properties are not indented):
+// script:
+// name: sample
 var normalizeStep = function(step) {
   if (typeof step === 'string') {
     return {
@@ -136,10 +175,31 @@ var normalizeStep = function(step) {
   return step;
 };
 
-var hasNullValue = function(o, key) {
-  return o[key] === null;
+// processPipelines takes a root object, and returns an array of normalized
+// pipelines. It will ignore any reserved keys, and it will delete the original
+// keys from the root object.
+var processPipelines = function(parsed) {
+  var pipelines = {};
+  var hasPipelines = false;
+
+  Object.keys(parsed)
+    .filter(shouldProcessPipelineKey)
+    .forEach(function(key) {
+      pipelines[key] = normalizePipeline(parsed[key]);
+      delete parsed[key];
+      hasPipelines = true;
+    });
+
+  if (hasPipelines) {
+    return pipelines;
+  }
+
+  return null;
 };
 
+// processExtraSteps takes a pipeline object, and returns an array of
+// normalized extra steps. It will ignore any reserved keys, and it will delete
+// the original keys from the root object.
 var processExtraSteps = function(pipeline) {
   var extraSteps = {};
   var hasExtraSteps = false;
@@ -157,9 +217,4 @@ var processExtraSteps = function(pipeline) {
   }
 
   return null;
-};
-
-var reservedPipelineKeys = ['box', 'services', 'steps', 'after-steps'];
-var shouldProcessExtraStepsKey = function(key) {
-  return reservedPipelineKeys.indexOf(key) === -1;
 };
